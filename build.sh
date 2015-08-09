92851eda9c24a60ce252a3064330746c95e4e9f3
Yaakov <pubgit@codingrange.com>
2015-08-09 07:53:56 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+gcc -o gpio_morse gpio_morse.c
+
---
+++ gpio_morse.c
@@ -0,0 +1,9 @@
+#include <wiringPi.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+	fprintf(stderr, "Not yet implemented.\n");
+	return -1;
+}
+
